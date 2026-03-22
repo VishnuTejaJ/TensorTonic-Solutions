3ed9687ee1f2820b629a01448c45a9c88529299a
VishnuTejaJ <117888210+VishnuTejaJ@users.noreply.github.com>
2026-03-22 22:46:55 +0530

    Add solution for positional-encoding

---
+++ positional-encoding/positional-encoding.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def positional_encoding(seq_len, d_model, base=10000.0):
+    """
+    Return PE of shape (seq_len, d_model) using sin/cos formulation.
+    Odd d_model -> last column is sin.
+    """
+    ans = np.zeros((seq_len,d_model))
+    for pos in range(seq_len):
+        for i in range(d_model):
+            if i%2==0:
+                ans[pos][i] = np.sin(pos/np.pow(base,i/d_model))
+            else:
+                ans[pos][i] = np.cos(pos/np.pow(base,(i-1)/d_model))
+    return ans
